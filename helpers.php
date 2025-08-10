c56ad732359abe5a545298d88a897d5457ea096a
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-08-10 16:34:50 +0700

    Adds PayOS signature verification
    Adds a helper function to verify the signature of PayOS transactions.
    
    The function sorts the transaction data, handles null and array values, and generates a signature using HMAC-SHA256.
    This signature is then compared to the provided transaction signature to ensure data integrity.

--- helpers.php
+++ helpers.php
@@ -12,3 +12,29 @@
 if (! class_exists(\Omnipay\Payos\Gateway::class)) {
     class_alias(\Juzaweb\PayOs\Gateway::class, \Omnipay\Payos\Gateway::class);
 }
+
+function verifySignaturePayos($transaction, $transactionSignature, $checksumKey): bool
+{
+    ksort($transaction);
+    $transactionStrArr = [];
+    foreach ($transaction as $key => $value) {
+        if (is_null($value) || in_array($value, ["undefined", "null"])) {
+            $value = "";
+        }
+
+        if (is_array($value)) {
+            $valueSortedElementObj = array_map(function ($ele) {
+                ksort($ele);
+                return $ele;
+            }, $value);
+            $value = json_encode($valueSortedElementObj, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE);
+        }
+        $transactionStrArr[] = $key . "=" . $value;
+    }
+
+    $transactionStr = implode("&", $transactionStrArr);
+
+    $signature = hash_hmac("sha256", $transactionStr, $checksumKey);
+
+    return $signature == $transactionSignature;
+}
