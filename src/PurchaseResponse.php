3eb5edcce8b1eb437c3919263975125ca36536c2
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-08-10 12:43:53 +0700

    Adds redirect functionality to purchase response
    Adds methods to handle redirect functionality after a purchase.
    
    Introduces `isRedirect`, `getRedirectUrl`, and `getEmbedUrl` methods to provide redirect information and embedded checkout URLs.

--- src/PurchaseResponse.php
+++ src/PurchaseResponse.php
@@ -18,4 +18,33 @@ class PurchaseResponse extends AbstractResponse
     {
         return isset($this->data['code']) && $this->data['code'] === '00';
     }
+
+    public function isRedirect(): true
+    {
+        return true;
+    }
+
+    /**
+     * Gets the redirect target url.
+     *
+     * @return string
+     */
+    public function getRedirectUrl(): ?string
+    {
+        return $this->data['checkoutUrl'] ?? null;
+    }
+
+    /**
+     * Get the redirect URL for the payment.
+     *
+     * @return string|null
+     */
+    public function getEmbedUrl(): ?string
+    {
+        if (! isset($this->data['checkoutUrl'])) {
+            return null;
+        }
+
+        return $this->data['checkoutUrl'] . '/?embedded=true';
+    }
 }
